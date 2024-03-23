d98302e66de29a9a6e8657e12256e83b4709b65b
gekoN <luckes195@gmail.com>
2024-03-23 17:39:25 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,21 @@
+# G3Archive-MassUnpack
+G3Archive-MassUnpacker is a Gothic 3 mass data file unpacker utilizing [G3Archive](https://github.com/gekonnn/G3Archive).
+
+> [!IMPORTANT]
+> G3Archive-MassUnpack requires .NET 7.0 installed
+
+# Installation
+Download the [latest release](https://github.com/gekonnn/G3Archive-MassUnpack/releases/) from Releases tab, after extracting the .zip file open the command prompt, `cd` into extracted directory and run the command following the usage below.
+
+# Usage and options
+### General usage:
+```
+G3Archive-MassUnpack [path-to-files] *[target-archive]
+```
+<i>* - optional
+### Example:
+```
+G3Archive-MassUnpack "C:\Program Files (x86)\Steam\steamapps\common\Gothic 3\Data" _compiledImage
+```
+# Dependencies
+- [G3Archive](https://github.com/gekonnn/G3Archive)
---
+++ src/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using G3Archive;
+
+namespace G3ArchiveMassUnpack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetArchive = null;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please specify archive files directory");
+                return;
+            }
+
+            if (args.Length > 1)
+            {
+                targetArchive = args[1];
+            }
+
+            ParsedOptions.Overwrite = true;
+            Logger.Quiet = true;
+
+            DirectoryInfo directory = new DirectoryInfo(args[0]);
+            if (!directory.Exists)
+            {
+                Console.WriteLine("Specified directory does not exist");
+                return;
+            }
+
+            FileInfo[] files = directory.GetFiles();
+            List<FileInfo> sortedFiles = SortFiles(files);
+            
+            foreach (FileInfo file in sortedFiles)
+            {
+                if (targetArchive != null && Path.GetFileNameWithoutExtension(file.Name) != targetArchive)
+                {
+                    continue;
+                }
+
+                G3Pak_Archive Archive = new G3Pak_Archive();
+                Archive.ReadArchive(file);
+                
+                Console.WriteLine($"Extracting {file.Name}...");
+                Archive.Extract(Path.Combine(Directory.GetCurrentDirectory(), "Extracted", Path.GetFileNameWithoutExtension(file.Name))).Wait();
+            }
+
+            Console.WriteLine("Extraction complete.");
+        }
+
+        static List<FileInfo> SortFiles(FileInfo[] files)
+        {
+            string[] extensions = { ".p", ".c", ".m", ".n" };
+            string[] generations = Enumerable.Range(0, 100).Select(i => i.ToString("D2")).ToArray();
+
+            List<FileInfo> sortedFiles = files.OrderBy(file =>
+            {
+                string extension = file.Extension.ToLower();
+                int extensionIndex = Array.IndexOf(extensions, extension.Substring(0, 2)) + 1;
+
+                string generation = extension.Substring(2);
+                int generationIndex = Array.IndexOf(generations, generation) + 1; 
+
+                return (extensionIndex * generations.Length) + generationIndex;
+            }).ToList();
+
+            return sortedFiles;
+        }
+    }
+}
